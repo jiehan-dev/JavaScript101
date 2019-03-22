bb662487d9c356d54c6b7ce8138a5f5d4418e5e3
Jiehan <jiehan.develop@gmail.com>
2019-03-22 13:28:45 +0800

    console.js

---
+++ Fundamentals/01_Console/console.js
@@ -0,0 +1,25 @@
+// Log to console
+console.log('Hello World');
+
+let obj = { a: 1, b: 2 };
+console.log(obj);
+console.table(obj);
+
+console.error('There is some error');
+// console.clear();
+
+console.warn('This is a warning');
+console.time('Hello');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+console.timeEnd('Hello');
+
+/*
+    multi
+    line
+    comments
+*/
