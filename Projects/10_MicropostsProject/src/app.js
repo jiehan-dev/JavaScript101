e89a3763d528835df76f248e0eeeadb8bb4f67c4
JieHan <jiehan.develop@gmail.com>
2019-07-25 12:42:54 +0800

    complete

--- Projects/10_MicropostsProject/src/app.js
+++ Projects/10_MicropostsProject/src/app.js
@@ -13,6 +13,9 @@ document.querySelector('#posts').addEventListener('click', deletePost);
 // Listen for edit state
 document.querySelector('#posts').addEventListener('click', enableEdit);
 
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
@@ -23,22 +26,38 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
-
+  const id = document.querySelector('#id').value;
   const data = {
     title,
     body
   };
+  if (title === '' || body === '') {
+    showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {
+    if (id === '') {
+      // Create Post
+      http
+        .post('http://localhost:3000/posts', data)
+        .then(data => {
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.clearFields();
 
-  // Create Post
-  http
-    .post('http://localhost:3000/posts', data)
-    .then(data => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    } else {
+      // Create Post
+      http
+        .put(`http://localhost:3000/posts/${id}`, data)
+        .then(data => {
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.clearFields();
 
-      getPosts();
-    })
-    .catch(err => console.log(err));
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
 }
 
 function deletePost(e) {
@@ -74,4 +93,11 @@ function enableEdit(e) {
 
     ui.fillForm(data);
   }
+}
+
+function cancelEdit(e) {
+  if (e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+  e.preventDefault();
 }
