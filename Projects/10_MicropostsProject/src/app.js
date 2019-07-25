a6888732d22d7aa0270e8a0be7d1fd3171195ad5
Jiehan <jiehan.develop@gmail.com>
2019-07-25 10:13:35 +0800

    WIP

---
+++ Projects/10_MicropostsProject/src/app.js
@@ -0,0 +1,77 @@
+import { http } from './http';
+import { ui } from './ui';
+
+// Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
+
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+function getPosts() {
+  http
+    .get('http://localhost:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  };
+
+  // Create Post
+  http
+    .post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+
+      getPosts();
+    })
+    .catch(err => console.log(err));
+}
+
+function deletePost(e) {
+  e.preventDefault();
+
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id; // html data attribute
+    if (confirm('Are you sure?')) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+}
+
+function enableEdit(e) {
+  e.preventDefault();
+
+  if (e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    };
+
+    ui.fillForm(data);
+  }
+}
